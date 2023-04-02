5dc0989845eb0ca9916a5c9495efb5cd6985e01d
김찬웅 <chanism@chanism-ui-MacBookPro.local>
2023-04-02 15:19:38 +0900

    Initial commit

---
+++ src/main/java/com/chanism/practicewebservice/PracticeWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.chanism.practicewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chanism/practicewebservice/PracticeWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanism.practicewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
