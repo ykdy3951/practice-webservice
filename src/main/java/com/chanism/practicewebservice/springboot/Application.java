69336c76d61624cfe22642801033b78d22aa4fd9
김찬웅 <chanism@chanism-ui-MacBookPro.local>
2023-04-02 18:39:32 +0900

    Chapter 2-2

--- src/main/java/com/chanism/practicewebservice/springboot/Application.java
+++ src/main/java/com/chanism/practicewebservice/springboot/Application.java
@@ -1,13 +1,12 @@
-package com.chanism.practicewebservice;
+package com.chanism.practicewebservice.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PracticeWebserviceApplication {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(PracticeWebserviceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
